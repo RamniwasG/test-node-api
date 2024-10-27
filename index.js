59124671afcad5d6c5810429d94a8520ea8bfb33
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-27 11:30:45 +0530

    minor cgange

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//     res.send("App is runing!");
-// })
+app.get('/', (req, res) => {
+    res.send("App is runing!");
+})
 
-// app.listen(port, () => {
-//     console.log("app is runing at " + port);
-// })
-
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('** Welcome to GlobalLogic!!!! **'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); 
+app.listen(port, () => {
+    console.log("app is runing at " + port);
+})
