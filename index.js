3733c8ca8aa26b98a0525ee81f4110d9f5c9fcce
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-27 12:31:20 +0530

    minor cgange

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 app.get('/home', (req, res) => {
     res.send("App is runing!");
